5b537b9f7b64f4eceeb931e415dddb3e8f9a1406
Suraj Chaudhary <43790524+su-477@users.noreply.github.com>
2022-09-26 00:11:32 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+﻿const gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    concat = require('gulp-concat'),
+    minjs = require('gulp-minify'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    del = require('del');
+
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        "scripts/vendor/cos.js",
+
+        // Global settings
+        "scripts/settings.js",
+
+        // Module text for all languages
+        "scripts/languages/*.js",
+
+        // Vendors
+        "scripts/vendor/dialReqs.js",
+        "scripts/vendor/jquery.min.js",
+        "scripts/vendor/jquery-ui.min.js",
+        "scripts/vendor/jquery.ui.touch-punch.min.js",
+        "scripts/vendor/scorm_api_min.js",
+
+        // All data for i18n, pagination and reporting.
+        "scripts/data/*.js",
+
+        // Controllers
+        "scripts/controllers/*.js",
+
+        // Components
+        "scripts/components/*.js",
+
+        // Utlity
+        "scripts/utility/*.js",
+
+        // Content, this runs as an initialiser.
+        "scripts/content.js"
+
+    ], { base: 'src' })
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("www/assets/"));
+});
+
+gulp.task('minify-js', function () {
+    return gulp.src('www/assets/scripts.js')
+        .pipe(minjs())
+        .pipe(gulp.dest('www/assets/'));
+});
+
+gulp.task('minify-css', () => {
+    return gulp.src('www/assets/main.css')
+        .pipe(cleanCSS())
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest('www/assets/'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('sass/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('www/assets/'));
+});
+
+gulp.task('watch:sass', function () {
+    gulp.watch('sass/**/*.scss', gulp.series('clean', 'scripts', 'sass'));
+});
+gulp.task('watch:scripts', function () {
+    gulp.watch('scripts/**/*.js', gulp.series('clean', 'scripts', 'sass'));
+});
+
+//Delete compiled script and css
+gulp.task('clean', function () {
+    return del(['www/assets/*.css', 'www/assets/*.js', 'www/assets/*.map']);
+});
+
+gulp.task('dev', gulp.series('clean', 'scripts', 'sass'));
+gulp.task('dist', gulp.series('dev', 'minify-js', 'minify-css'));
---
+++ setup.js
@@ -0,0 +1,71 @@
+const prompt = require("prompt");
+const replace = require("replace-in-file");
+
+const uuid = require("uuid");
+const gulp = require("gulp");
+require('./gulpfile.js')
+
+// prompt the user for different config
+prompt.start();
+
+const properties = [
+    // {
+    //     name: 'ProjectID',
+    //     description: 'Enter the Project ID (UUID/GUID)',
+    //     validator: /^[a-fA-F0-9\s\-]+$/,
+    //     warning: 'ProjectID must be a valid UUID'
+    // },
+    {
+        name: 'Project',
+        description: '',
+        validator: /^[0-9\s\-]+$/,
+        warning: 'Project name required '
+    },
+];
+
+function r(options) {
+    try {
+        let changedFiles = replace.sync(options);
+    } catch (e) { }
+}
+
+prompt.start();
+
+prompt.get(properties, function (err, result) {
+    if (err) { return onErr(err); }
+
+    let complete = false;
+
+    for (let i = 0; i < properties.length; i++) {
+        const options = {
+            files: 'scripts/settings.js',
+            from: '%' + properties[i].name + '%',
+            to: result[properties[i].name]
+        }
+
+        r(options);
+    }
+
+    const id = uuid.v4();
+
+    const options = {
+        files: 'scripts/settings.js',
+        from: '%ProjectID%',
+        to: id
+    }
+    r(options);
+
+    gulp.task('dev')();
+    console.log("Settings updated. Please update values in www/imsmanifest.xml.");
+
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
+
+const settings = {
+    "ProjectID": "%ProjectID%",
+}
+
